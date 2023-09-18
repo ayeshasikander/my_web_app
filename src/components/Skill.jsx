68b19d2f1352f8303c2a8cf6c94185a68cd89fb8
ayeshasikander <sikanderayesha46@gmail.com>
2023-09-18 09:38:01 +0500

    add backend tool

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -9,6 +9,7 @@ import skill4 from '../assets/react3-bg.png'
 import skill5 from '../assets/btsp.png'
 import skill6 from '../assets/rebs.png'
 import skill7 from '../assets/vscode.png'
+import skill8 from '../assets/strapi-1.png'
 import './style.css';
 
 const Skill = (props) => {
@@ -39,8 +40,8 @@ const Skill = (props) => {
                         <div className='skill-box'>
                             <h2 style={{ color: "white" }}>Skills & Tools</h2>
                             <hr style={{ height: "7px", width: "100%", color: "#0497ad" }}></hr>
-                            
-                           <p>Armed with a versatile toolbox, I transform ideas into captivating digital experiences.</p>
+
+                            <p>Armed with a versatile toolbox, I transform ideas into captivating digital experiences.</p>
                             <Carousel
                                 swipeable={false}
                                 draggable={false}
@@ -61,47 +62,54 @@ const Skill = (props) => {
                                 className='skill-slider'>
                                 <div className='item carousel-item'>
                                     <div className='circular-image'>
-                                        <img src={skill1} style={{width:"79%"}} alt="HTML5" />
+                                        <img src={skill1} style={{ width: "79%" }} alt="HTML5" />
                                     </div>
                                     <h5 style={{ color: "white" }}>HTML</h5>
 
                                 </div>
                                 <div className='item carousel-item'>
                                     <div className='circular-image'>
-                                        <img src={skill2} style={{width:"60%"}} alt="CSS" />
+                                        <img src={skill2} style={{ width: "60%" }} alt="CSS" />
                                     </div>
                                     <h5 style={{ color: "white" }}>CSS</h5>
                                 </div>
                                 <div className='item carousel-item'>
                                     <div className='circular-image'>
-                                        <img src={skill3} style={{width:"60%"}}alt="Js" />
+                                        <img src={skill3} style={{ width: "60%" }} alt="Js" />
                                     </div>
                                     <h5 style={{ color: "white" }}>Javascript</h5>
                                 </div>
                                 <div className='item carousel-item'>
                                     <div className='circular-image'>
-                                        <img src={skill4} style={{width:"85%"}} alt="React" />
-                                    </div>
-                                    <h5 style={{ color: "white" }}>React Js</h5>
-                                </div>
-                                <div className='item carousel-item'>
-                                    <div className='circular-image'>
-                                        <img src={skill5} style={{width:"95%"}} alt="React" />
+                                        <img src={skill5} style={{ width: "95%" }} alt="React" />
                                     </div>
                                     <h5 style={{ color: "white" }}>Bootstrap</h5>
                                 </div>
                                 <div className='item carousel-item'>
                                     <div className='circular-image'>
-                                        <img src={skill6} style={{width:"78%",margin:"10px"}} alt="React" />
+                                        <img src={skill4} style={{ width: "85%" }} alt="React" />
+                                    </div>
+                                    <h5 style={{ color: "white" }}>React Js</h5>
+                                </div>
+
+                                <div className='item carousel-item'>
+                                    <div className='circular-image'>
+                                        <img src={skill6} style={{ width: "78%", margin: "10px" }} alt="React" />
                                     </div>
                                     <h5 style={{ color: "white" }}>React-Bootstrap</h5>
                                 </div>
                                 <div className='item carousel-item'>
                                     <div className='circular-image'>
-                                        <img src={skill7} style={{width:"78%",margin:"10px"}} alt="React" />
+                                        <img src={skill8} style={{ width: "70%", margin: "20px" }} alt="Strapi CMS" />                                    </div>
+                                    <h5 style={{ color: "white" }}>Strapi CMS</h5>
+                                </div>
+                                <div className='item carousel-item'>
+                                    <div className='circular-image'>
+                                        <img src={skill7} style={{ width: "78%", margin: "10px" }} alt="React" />
                                     </div>
                                     <h5 style={{ color: "white" }}>Visual Studio Code</h5>
                                 </div>
+
                             </Carousel>
                         </div>
                     </Col>
